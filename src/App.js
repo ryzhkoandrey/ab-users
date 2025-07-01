56476e057562f4ae81ab45ecce03d0309396c16a
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-07-01 10:57:08 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,20 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+   const [users, setUsers] = React.useState([]);
+
+   React.useEffect(() => {
+      fetch('https://reqres.in/api/users')
+         .then((res) => res.json())
+         .then((json) => {
+            setUsers(json.data);
+         })
+         .catch((err) => {
+            console.warn(err);
+            alert('Ошибка при получении пользователей');
+         });
+   }, []);
+
    return (
       <div className="App">
          <Users />
