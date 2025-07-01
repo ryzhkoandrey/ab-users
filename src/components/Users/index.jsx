c7630465b96bf3c1d5154300ee18bab6df6fe9e2
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-07-01 18:04:44 +0300

    Update index.jsx

--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -9,7 +9,12 @@ export const Users = ({ items, isLoading, searchValue, onChangeSearchValue }) =>
             <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
             </svg>
-            <input type="text" placeholder="Найти пользователя..." />
+            <input
+               value={searchValue}
+               onChange={onChangeSearchValue}
+               type="text"
+               placeholder="Найти пользователя..."
+            />
          </div>
 
          {isLoading ? (
@@ -22,9 +27,17 @@ export const Users = ({ items, isLoading, searchValue, onChangeSearchValue }) =>
             </div>
          ) : (
             <ul className="users-list">
-               {items?.map((obj) => (
-                  <User key={obj.id} {...obj} />
-               ))}
+               {items
+                  .filter((obj) => {
+                     const fullName = (obj.first_name + obj.last_name).toLowerCase();
+                     return (
+                        fullName.includes(searchValue.toLowerCase()) ||
+                        obj.email.toLowerCase().includes(searchValue.toLowerCase())
+                     );
+                  })
+                  .map((obj) => (
+                     <User key={obj.id} {...obj} />
+                  ))}
             </ul>
          )}
 
